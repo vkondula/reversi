78d51b9d7065bc0ed8e92d73ed80090de9a1f63b
xkrat <krajnakmatto@gmail.com>
2016-05-02 20:54:21 +0200

    added simple version of menu

--- src/ija/project2015/boardgame/game/TemplateGUI.java
+++ src/ija/project2015/boardgame/game/TemplateGUI.java
@@ -4,90 +4,174 @@ package ija.project2015.boardgame.game;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
-
-import java.awt.BorderLayout;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.plaf.basic.BasicMenuBarUI;
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.GridLayout;
+
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
-import java.awt.FlowLayout;
-import java.awt.GridBagLayout;
+import javax.swing.JComponent;
+import java.awt.Graphics;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
 
 public class TemplateGUI extends JFrame implements ActionListener {
 	
+	private JButton btnUndo = new JButton("Undo");
 	
 	public TemplateGUI(int x) {
 		
-		int size = 60;
-		int offset = 150;
+		final int size = 60;
+		final int paneSize = x*size;
+		final int windowSize = x*size+30;
 		
-		this.setMinimumSize(new Dimension(x*size + offset,x*size+ offset));
+		this.getContentPane().setMinimumSize(new Dimension(paneSize,paneSize));
+		this.getContentPane().setBackground(Color.DARK_GRAY);
+		this.setMinimumSize(new Dimension(windowSize,windowSize));
 		this.setTitle("REVERSI");
-		getContentPane().setLayout(new MigLayout("", "[456.00,grow][]", "[420.00,fill]"));
-		this.getContentPane().setMinimumSize(new Dimension(x*size + offset,x*size + 50));
 		
-		JButton[][] buttons = new JButton[8][8];	
+		this.getContentPane().setLayout(new MigLayout("", "[grow]", "[grow]"));
+		
+		//JPanel controlPanel = new JPanel();
+		
+		//controlPanel.add(btnUndo);
+		
+		JButton[][] btnFields = new JButton[x][x];	
 		JPanel boardPanel = new JPanel();
-		JPanel controlPanel = new JPanel();
-		
-		boardPanel.setLayout(new GridLayout(8, 8, 1, 1));
-		boardPanel.setMinimumSize(new Dimension(x*size,x*size));
-		boardPanel.setSize(new Dimension(x*size, x*size));	
-		
-		
-		for	(int i=0; i<8; i++)
+	
+		boardPanel.setLayout(new MigLayout("gap 1px", "[" + String.valueOf(paneSize)+ ", grow]", "[" + String.valueOf(paneSize)+ ", grow]"));
+				
+			
+		for	(int i=0; i<x; i++)
 		{
-			for	(int j=0; j<8; j++)
+			for	(int j=0; j<x; j++)
 			{	
-				buttons[i][j] = new JButton(String.valueOf(i)+String.valueOf(j));
-				buttons[i][j].setBackground(new Color(0,102,153));
-				boardPanel.add(buttons[i][j]);
+				btnFields[i][j] = new JButton(String.valueOf(i)+String.valueOf(j));
+				btnFields[i][j].setBackground(new Color(0,102,153));
+				boardPanel.add(btnFields[i][j], "cell " + String.valueOf(i)+ " " + String.valueOf(j) +" ,grow");
+				
 				
 			}
 		}
 		
-		controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.Y_AXIS));
-		controlPanel.setMinimumSize(new Dimension(offset,x*size));
-		controlPanel.setSize(new Dimension(offset, x*size));	
-		
-		JButton btnNewGame = new JButton("New Game");
-		JButton btnSaveGame = new JButton("Save Game");
-		JButton btnLoadGame = new JButton("Load Game");
-		JButton btnFreezeStones = new JButton("Freeze Stones");
-		
-		Dimension buttonGap = new Dimension(0,5);
-		
-		
-		controlPanel.add(btnNewGame);
-		controlPanel.add(Box.createRigidArea(buttonGap));
-		controlPanel.add(btnSaveGame);
-		controlPanel.add(Box.createRigidArea(buttonGap));
-		controlPanel.add(btnLoadGame);
-		controlPanel.add(Box.createRigidArea(buttonGap));
-		controlPanel.add(btnFreezeStones);
-		
+		//this.getContentPane().add(controlPanel,"cell 0 0,growx");
 		this.getContentPane().add(boardPanel, "cell 0 0,baseline");
-		this.getContentPane().add(controlPanel, "cell 1 0,baseline");
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		menuBar.setLayout(new MigLayout("","[grow]"));
+		
+		btnUndo.setBackground(Color.GRAY);
+		menuBar.add(this.btnUndo,"cell 0 0,baseline");
+		
+		JMenu menu = new JMenu("MENU");
+		menuBar.add(menu, "cell 1 0,center");
+		
+		JMenuItem SaveGame = new JMenuItem("Save Game");
+		menu.add(SaveGame);
+		JMenuItem LoadGame = new JMenuItem("Load Game");
+		menu.add(LoadGame);
+		
+		menu.addSeparator();
+		JLabel lblOponent = new JLabel("Select Oponent");
+		menu.add(lblOponent);
+		
+		ButtonGroup selectOponent = new ButtonGroup();
+		JRadioButtonMenuItem oponentComputer = new JRadioButtonMenuItem("Computer");
+		
+		oponentComputer.setSelected(true);
+		selectOponent.add(oponentComputer);
+		menu.add(oponentComputer);
+
+		JRadioButtonMenuItem oponentPlayer = new JRadioButtonMenuItem("Another Player");
+		
+		selectOponent.add(oponentPlayer);
+		menu.add(oponentPlayer);
+		
+		menu.addSeparator();
+		
+		JLabel lblSize = new JLabel("Select Size");
+		menu.add(lblSize);
+
+		ButtonGroup selectFieldSize = new ButtonGroup();
+		JRadioButtonMenuItem sizeSix = new JRadioButtonMenuItem("6x6");
+		
+		selectFieldSize.add(sizeSix);
+		menu.add(sizeSix);
+
+		JRadioButtonMenuItem sizeEight = new JRadioButtonMenuItem("8x8");
+		
+		sizeEight.setSelected(true);
+		selectFieldSize.add(sizeEight);
+		menu.add(sizeEight);
+		
+		JRadioButtonMenuItem sizeTen = new JRadioButtonMenuItem("10x10");
+		
+		selectFieldSize.add(sizeTen);
+		menu.add(sizeTen);
+		
+		JRadioButtonMenuItem sizeTwelve = new JRadioButtonMenuItem("12x12");
+		
+		selectFieldSize.add(sizeTwelve);
+		menu.add(sizeTwelve);
+
+		menu.addSeparator();
+
+		JLabel lblFreeze = new JLabel("Stone Freezing");
+		menu.add(lblFreeze);
+
+		ButtonGroup selectFreeze = new ButtonGroup();
+		JRadioButtonMenuItem freezeI = new JRadioButtonMenuItem("Freeze I");
+		
+		selectFreeze.add(freezeI);
+		menu.add(freezeI);
+
+		JRadioButtonMenuItem freezeB = new JRadioButtonMenuItem("Freeze B");
+		
+		selectFreeze.add(freezeB);
+		menu.add(freezeB);
+		
+		JRadioButtonMenuItem freezeC = new JRadioButtonMenuItem("Freeze C");
+		
+		selectFreeze.add(freezeC);
+		menu.add(freezeC);
+		
+		JRadioButtonMenuItem freezeOff = new JRadioButtonMenuItem("Freeze Off");
+		
+		freezeOff.setSelected(true);
+		selectFreeze.add(freezeOff);
+		menu.add(freezeOff);
+
+
+		menu.addSeparator();
+		
+		JMenuItem QuitGame = new JMenuItem("Quit Game");
+		menu.add(QuitGame);
+		
+		
+		JLabel info = new JLabel("White's turn");
+		menuBar.add(info,"cell 2 0,right");
+		
+		menuBar.setUI ( new BasicMenuBarUI (){
+		    public void paint ( Graphics g, JComponent c ){
+		       g.setColor ( Color.GRAY);
+		       g.fillRect ( 0, 0, c.getWidth (), c.getHeight () );
+		    }
+		} );
+		
 		this.setVisible(true);	
+		
 	}
 	
 	
-	/*Comment*/
-
 	/**
 	 * Launch the application.
 	 */
@@ -97,12 +181,11 @@ public class TemplateGUI extends JFrame implements ActionListener {
 		new TemplateGUI(8);
 	}
 
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
-
-	
 	
 }
