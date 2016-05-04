8b1546f12452549a3a23c0c376b25535ffcfc0db
xkrat <krajnakmatto@gmail.com>
2016-05-05 00:55:30 +0200

    added icons, gui merged, with game, added new menu, also fixed problem with stones - they were changing size of board FML

--- src/ija/project2015/reversi/ReversiGUI.java
+++ src/ija/project2015/reversi/ReversiGUI.java
@@ -8,31 +8,31 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JRadioButtonMenuItem;
-import javax.swing.JSlider;
+import javax.swing.JSeparator;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingConstants;
-import javax.swing.plaf.basic.BasicMenuBarUI;
-
-import com.sun.org.apache.xalan.internal.xsltc.compiler.Template;
 
 import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
-import javax.swing.ButtonModel;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import javax.swing.JComponent;
-import java.awt.Graphics;
 import java.awt.TextField;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
-
 import ija.project2015.boardgame.game.Game;
 import ija.project2015.boardgame.board.Board;
 import ija.project2015.boardgame.board.Field;
 import ija.project2015.boardgame.game.Rules;
 import ija.project2015.reversi.ReversiRules;
+import jdk.nashorn.internal.ir.WhileNode;
 import ija.project2015.reversi.AI;
 import ija.project2015.boardgame.game.Player;
 import ija.project2015.reversi.FieldButton;
@@ -45,30 +45,45 @@ public class ReversiGUI extends JFrame implements ActionListener {
 	private JButton[][] btnFields;	
 	private JPanel boardPanel;
 	private JMenuBar menuBar;
-	private JMenu menu;
-	private JMenuItem createGame;
-	private JMenuItem saveGame;
-	private JMenuItem loadGame;
-	private JMenuItem exitGame;
-	private JLabel lblOponent;
-	private JLabel lblSize;
+	
+	private JPanel controlPanel;
+
+	private JLabel gameSettings;
+	private JLabel oponentSettings;
 	private ButtonGroup selectOponent;
 	private JRadioButtonMenuItem oponentAlgoritm1;
 	private JRadioButtonMenuItem oponentAlgoritm2;
 	private JRadioButtonMenuItem oponentPlayer;
+
+	private JLabel sizeSettings;
 	private ButtonGroup selectFieldSize;
 	private JRadioButtonMenuItem sizeSix;
 	private JRadioButtonMenuItem sizeEight;
 	private JRadioButtonMenuItem sizeTen;
 	private JRadioButtonMenuItem sizeTwelve;
+	
+	private ButtonGroup selectStoneColor;
+	private JRadioButtonMenuItem blackStones;
+	private JRadioButtonMenuItem whiteStones;
+	private JLabel stoneSettings;
+
+	private JButton menu;
+	private JButton createGame;
+	private JButton loadGame;
+	private JButton saveGame;
 	private JLabel info;
 	private JCheckBox freezer;
-	private JLabel I;
-	private JLabel B;
-	private JLabel C;
-	private TextField setI;
-	private TextField setB;
-	private TextField setC;
+	private JLabel lblI;
+	private JLabel lblB;
+	private JLabel lblC;
+	private JSpinner spinB;
+	private JSpinner spinC;
+	private JSpinner spinI;
+	
+
+	private Color customBlueBackground;
+	private ImageIcon blackStone = new ImageIcon("images/black.png");
+	private ImageIcon whiteStone = new ImageIcon("images/white.png");
 	
 	private Rules rules;
 	private Board board;
@@ -77,12 +92,27 @@ public class ReversiGUI extends JFrame implements ActionListener {
 	private Player black;
 	private boolean playing = true;
 	
+	private int B;	//freezer values
+	private int C;
+	private int I;
 	
-	public ReversiGUI(int x, int alg) {
+	
+	public ReversiGUI(int x, int alg, int b, int c, int y) {
 		
-		final int itemWidth = 200;
-		final int itemHeight = 30; 
 		this.boardSize = x;
+		this.B = b;
+		this.C = c;
+		this.I = y;
+		
+		JSeparator optionSep = new JSeparator();
+		JSeparator optionSep2 = new JSeparator();
+		
+		customBlueBackground = new Color(0,102,153);
+		blackStone = new ImageIcon("images/black.png");
+		whiteStone = new ImageIcon("images/white.png");
+		
+		this.setResizable(false);
+		
 		// Game related objects
 		rules = new ReversiRules(x);
 		board = new Board(rules);
@@ -96,87 +126,15 @@ public class ReversiGUI extends JFrame implements ActionListener {
 		game.addPlayer(black);
 		game.addPlayer(white);
 			
-		
 		btnUndo = new JButton("Undo");
+		btnUndo.addActionListener(this);
+		
 		boardPanel = new JPanel();
-		menuBar = new JMenuBar();
-		menu = new JMenu("MENU");
+		info = new JLabel("Welcome");
+		info.setHorizontalAlignment(SwingConstants.RIGHT);
+		info.setPreferredSize(new Dimension(200,20));
 		
-		menu.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		createGame = new JMenuItem("Create Game");
-		createGame.setPreferredSize(new Dimension(itemWidth, itemHeight));
-		createGame.addActionListener(this);
-		
-		saveGame = new JMenuItem("Save Game");
-		saveGame.setPreferredSize(new Dimension(itemWidth, itemHeight));
-		saveGame.addActionListener(this);
-		
-		loadGame = new JMenuItem("Load Game");
-		loadGame.setPreferredSize(new Dimension(itemWidth, itemHeight));
-		loadGame.addActionListener(this);
-		
-		exitGame = new JMenuItem("Exit Game");
-		exitGame.setPreferredSize(new Dimension(itemWidth, itemHeight));
-		exitGame.addActionListener(this);
-		
-		lblOponent = new JLabel("Select Oponent");
-		lblOponent.setPreferredSize(new Dimension(itemWidth,itemHeight));
-		
-		selectOponent = new ButtonGroup();
-		oponentAlgoritm1 = new JRadioButtonMenuItem("Easy");
-		oponentAlgoritm1.setPreferredSize(new Dimension(itemWidth, itemHeight));
-		
-		oponentAlgoritm2 = new JRadioButtonMenuItem("Hard");
-		oponentAlgoritm2.setPreferredSize(new Dimension(itemWidth, itemHeight));
-		
-		oponentPlayer = new JRadioButtonMenuItem("Another Player");
-		oponentPlayer.setPreferredSize(new Dimension(itemWidth, itemHeight));
-		
-		lblSize = new JLabel("Select Size");
-		selectFieldSize = new ButtonGroup();
-		
-		sizeSix = new JRadioButtonMenuItem("6x6");
-		sizeSix.addActionListener(this);
-		sizeSix.setPreferredSize(new Dimension(itemWidth, itemHeight));
-		
-		sizeEight = new JRadioButtonMenuItem("8x8");
-		sizeEight.setPreferredSize(new Dimension(itemWidth, itemHeight));
-		sizeEight.addActionListener(this);
-		
-		sizeTen = new JRadioButtonMenuItem("10x10");
-		sizeTen.setPreferredSize(new Dimension(itemWidth, itemHeight));
-		sizeTen.addActionListener(this);
-		
-		sizeTwelve = new JRadioButtonMenuItem("12x12");
-		sizeTwelve.setPreferredSize(new Dimension(itemWidth, itemHeight));
-		sizeTwelve.addActionListener(this);
-		
-		info = new JLabel("Black player's turn");
-		freezer = new JCheckBox("Freeze stones", false);
-		freezer.setPreferredSize(new Dimension(itemWidth, itemHeight));
-		
-		I = new JLabel("I");
-		I.setPreferredSize(new Dimension(itemWidth, itemHeight));
-		B = new JLabel("B");
-		B.setPreferredSize(new Dimension(itemWidth, itemHeight));
-		C = new JLabel("C");
-		C.setPreferredSize(new Dimension(itemWidth, itemHeight));
-		
-//		setI = new TextField("Time between freezes");
-//		setI.setPreferredSize(new Dimension(itemWidth,itemHeight));
-//		setI.addActionListener(this);
-//		setI.setEnabled(false);
-//		
-//		setC = new TextField("Number Of Stones");
-//		setC.setPreferredSize(new Dimension(itemWidth,itemHeight));
-//		setC.addActionListener(this);
-//		
-//		setB = new TextField("Freeze Time");
-//		setB.setPreferredSize(new Dimension(itemWidth,itemHeight));
-//		setB.addActionListener(this);
-		
-		final int size = 60;
+		final int size = 70;
 		final int paneSize = x*size;
 		final int windowSize = x*size+30;
 		
@@ -184,7 +142,7 @@ public class ReversiGUI extends JFrame implements ActionListener {
 		this.getContentPane().setBackground(Color.DARK_GRAY);
 		this.setMinimumSize(new Dimension(windowSize,windowSize));
 		this.setTitle("REVERSI");
-		
+	
 		this.getContentPane().setLayout(new MigLayout("", "[grow]", "[grow]"));
 		
 	
@@ -195,100 +153,162 @@ public class ReversiGUI extends JFrame implements ActionListener {
 		{
 			for	(int j=0; j<x; j++)
 			{	
-				btnFields[i][j] = new FieldButton(String.valueOf(i)+String.valueOf(j), i+1, j+1);
+				btnFields[i][j] = new FieldButton("", i+1, j+1);
 				btnFields[i][j].setBackground(new Color(0,102,153));
 				btnFields[i][j].addActionListener(this);
+				btnFields[i][j].setMargin(null);
 				boardPanel.add(btnFields[i][j], "cell " + String.valueOf(i)+ " " + String.valueOf(j) +" ,grow");
 			}
 		}
 		
 		this.getContentPane().add(boardPanel, "cell 0 0,baseline");
 		
-		setJMenuBar(menuBar);
-		
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);	
 		menuBar.setLayout(new MigLayout("","[grow]"));
-		
-		btnUndo.setBackground(Color.GRAY);
-		btnUndo.addActionListener(this);
 		menuBar.add(this.btnUndo,"cell 0 0,baseline");
-		
-		menuBar.add(menu, "cell 1 0,center");
-		
-		menu.add(createGame);
-		menu.add(saveGame);
-		menu.add(loadGame);
-		
-		menu.addSeparator();
-		menu.add(lblOponent);
-		
-		oponentAlgoritm1.setSelected(true);
+
+		menu = new JButton("Menu");
+		menu.addActionListener(this);
+		menuBar.add(menu, "cell 1 0,right");
+
+		controlPanel = new JPanel(new MigLayout("alignx center, fillx "));
+		controlPanel.setBackground(Color.red);
+		controlPanel.setPreferredSize(this.getSize());
+		controlPanel.setBackground(customBlueBackground);
+		controlPanel.setBorder(BorderFactory.createLineBorder(Color.WHITE, 4));
+
+		createGame = new JButton("Create Game");
+		createGame.addActionListener(this);
+		loadGame = new JButton("Load Game");
+		loadGame.addActionListener(this);
+		saveGame = new JButton("Save Game");
+		saveGame.addActionListener(this);
+
+		gameSettings = new JLabel("Game");
+		gameSettings.setForeground(Color.WHITE);
+
+		controlPanel.add(gameSettings, "cell 0 0, left");
+		controlPanel.add(createGame, "cell 0 1,center");
+		controlPanel.add(loadGame, "cell 1 1,center");
+		controlPanel.add(saveGame, "cell 2 1,center, span");
+
+		selectOponent = new ButtonGroup();
+		oponentAlgoritm1 = new JRadioButtonMenuItem("Algoritm 1");
+		oponentAlgoritm1.addActionListener(this);
+		oponentAlgoritm1.setBackground(customBlueBackground);
+		oponentAlgoritm1.setForeground(Color.WHITE);
+
+		oponentAlgoritm2 = new JRadioButtonMenuItem("Algoritm 1");
+		oponentAlgoritm2.addActionListener(this);
+		oponentAlgoritm2.setBackground(customBlueBackground);
+		oponentAlgoritm2.setForeground(Color.WHITE);
+
+		oponentPlayer = new JRadioButtonMenuItem("Another Player");
+		oponentPlayer.addActionListener(this);
+		oponentPlayer.setBackground(customBlueBackground);
+		oponentPlayer.setForeground(Color.WHITE);
+
 		selectOponent.add(oponentAlgoritm1);
-		menu.add(oponentAlgoritm1);
-		
 		selectOponent.add(oponentAlgoritm2);
-		menu.add(oponentAlgoritm2);
-
 		selectOponent.add(oponentPlayer);
-		menu.add(oponentPlayer);
 		
-		menu.addSeparator();
-		menu.add(lblSize);
+		controlPanel.add(optionSep, "growx, wrap, span 3");
+		
+		oponentSettings = new JLabel("Select oponent");
+		oponentSettings.setForeground(Color.white);
+		
+		controlPanel.add(oponentSettings, "cell 0 4, left, gapy 30px");
+		controlPanel.add(oponentPlayer, "cell 0 5, left");
+		controlPanel.add(oponentAlgoritm1, "cell 0 6, left");
+		controlPanel.add(oponentAlgoritm2, "cell 0 7, left");
 
-		
-		selectFieldSize.add(sizeSix);
-		menu.add(sizeSix);
+		sizeSettings = new JLabel("Select board size");
+		sizeSettings.setForeground(Color.WHITE);
+
+		selectFieldSize = new ButtonGroup();
+		sizeSix = new JRadioButtonMenuItem("6x6");
+		sizeSix.setBackground(customBlueBackground);
+		sizeSix.setForeground(Color.WHITE);
+		sizeSix.addActionListener(this);
+
+		sizeEight = new JRadioButtonMenuItem("8x8");
+		sizeEight.setBackground(customBlueBackground);
+		sizeEight.setForeground(Color.WHITE);
+		sizeEight.addActionListener(this);
+
+		sizeTen = new JRadioButtonMenuItem("10x10");
+		sizeTen.setBackground(customBlueBackground);
+		sizeTen.setForeground(Color.WHITE);
+		sizeTen.addActionListener(this);
+
+		sizeTwelve = new JRadioButtonMenuItem("12x12");
+		sizeTwelve.setBackground(customBlueBackground);
+		sizeTwelve.setForeground(Color.WHITE);
+		sizeTwelve.addActionListener(this);
 
 		selectFieldSize.add(sizeEight);
-		menu.add(sizeEight);
-		
-	
+		selectFieldSize.add(sizeSix);
 		selectFieldSize.add(sizeTen);
-		menu.add(sizeTen);
-		
 		selectFieldSize.add(sizeTwelve);
-		menu.add(sizeTwelve);
 
-		menu.addSeparator();
+		controlPanel.add(sizeSettings, "cell 1 4, Left , gapy 30px");
+		controlPanel.add(sizeSix, "cell 1 5, center");
+		controlPanel.add(sizeEight, "cell 1 6, center");
+		controlPanel.add(sizeTen, "cell 1 7, center");
+		controlPanel.add(sizeTwelve, "cell 1 8, center");
+
+		freezer = new JCheckBox("Enable Freeze");
+		freezer.setForeground(Color.WHITE);
+		freezer.setBackground(customBlueBackground);
+		freezer.addActionListener(this);
+		controlPanel.add(freezer,"cell 2 4, right, gapy 30px");
+
+		SpinnerModel spinnerModel = new SpinnerNumberModel(10,0,20,1);
+
+		lblI = new JLabel("I:");
+		lblI.setForeground(Color.white);
+		spinI = new JSpinner(spinnerModel);
+		controlPanel.add(lblI,"cell 2 5, split 2, right ");
+		controlPanel.add(spinI);
+
+		lblB = new JLabel("B:");
+		lblB.setForeground(Color.WHITE);
+		spinB = new JSpinner(spinnerModel);
+		controlPanel.add(lblB,"cell 2 6, split 2, right");
+		controlPanel.add(spinB);
+
+		lblC = new JLabel("C:");
+		lblC.setForeground(Color.white);
+		spinC = new JSpinner(spinnerModel);
+		controlPanel.add(lblC,"cell 2 7, split 2, right");
+		controlPanel.add(spinC);
+
+		controlPanel.add(optionSep2, "cell 0 9, growx, wrap, span 3");
 		
-		menu.add(freezer);
-		menu.add(I);
-		JSlider Islider = new JSlider(JSlider.HORIZONTAL, 0, 20, 10);
-		Islider.setMajorTickSpacing(5);
-		Islider.setMinorTickSpacing(1);
-		Islider.setPaintLabels(true);
-		Islider.setPaintTicks(true);
+		stoneSettings = new JLabel("Choose side");
+		stoneSettings.setForeground(Color.white);
 		
-		menu.add(Islider);
-		JSlider Cslider = new JSlider(JSlider.HORIZONTAL, 0, 20, 10);
-		Cslider.setMajorTickSpacing(5);
-		Cslider.setMinorTickSpacing(1);
-		Cslider.setPaintLabels(true);
-		Cslider.setPaintTicks(true);
+		controlPanel.add(stoneSettings,"cell 1 10, left, gapy 30px");
 		
-		menu.add(C);
-		menu.add(Cslider);
-		JSlider Bslider = new JSlider(JSlider.HORIZONTAL, 0, 20, 10);
-		Bslider.setMajorTickSpacing(5);
-		Bslider.setMinorTickSpacing(1);
-		Bslider.setPaintLabels(true);
-		Bslider.setPaintTicks(true);
+		selectStoneColor = new ButtonGroup();
+		whiteStones = new JRadioButtonMenuItem(whiteStone);
+		whiteStones.setBackground(customBlueBackground);
+		whiteStones.addActionListener(this);
+		selectStoneColor.add(whiteStones);
 		
-		menu.add(B);
-		menu.add(Bslider);
+		blackStones = new JRadioButtonMenuItem(blackStone);
+		blackStones.setBackground(customBlueBackground);
+		blackStones.addActionListener(this);
+		selectStoneColor.add(blackStones);
+		blackStones.setSelected(true);
 		
-		menu.addSeparator();
-		menu.add(exitGame);
+		controlPanel.add(blackStones, "cell 1 11,  right ");
+		controlPanel.add(whiteStones, "cell 1 12,  right ");
 		
 		menuBar.add(info,"cell 2 0,right");
 		
-		menuBar.setUI ( new BasicMenuBarUI (){
-		    public void paint ( Graphics g, JComponent c ){
-		       g.setColor ( Color.GRAY);
-		       g.fillRect ( 0, 0, c.getWidth (), c.getHeight () );
-		    }
-		} );
-		
-		switch (this.boardSize) {
+		switch (x) {
 		case 6:
 			sizeSix.setSelected(true);
 			break;
@@ -305,6 +325,34 @@ public class ReversiGUI extends JFrame implements ActionListener {
 			break;
 		}
 		
+		switch (alg) {
+		case 0:
+			oponentAlgoritm1.setSelected(true);
+			break;
+		case 1:
+			oponentAlgoritm2.setSelected(true);
+			break;
+		case 2:
+			oponentPlayer.setSelected(true);
+			break;
+		default:
+			break;
+		}
+		
+		if(this.B != 0 || this.C != 0 || this.I != 0)
+		{
+			freezer.setSelected(true);
+			spinB.setValue(this.B);
+			spinC.setValue(this.C);
+			spinI.setValue(this.I);
+		}
+		else
+		{
+			freezer.setSelected(false);
+			spinB.setEnabled(false);
+			spinC.setEnabled(false);
+			spinI.setEnabled(false);
+		}
 		this.setVisible(true);	
 	
 		// GAME RELATED CODE
@@ -321,15 +369,6 @@ public class ReversiGUI extends JFrame implements ActionListener {
 	}
 	
 	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		
-		System.out.println("Hello World");
-		new ReversiGUI(8, 1);
-	}
-
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -368,13 +407,10 @@ public class ReversiGUI extends JFrame implements ActionListener {
 			else if (oponentAlgoritm2.isSelected()){
 				alg = 2;
 			}
-			new ReversiGUI(this.boardSize, alg);
+			new ReversiGUI(this.getBoardSize(), alg, 
+					(int)this.spinB.getValue(), (int)this.spinC.getValue(), (int)this.spinI.getValue());
 		}
 		
-		if (e.getSource() == this.exitGame)
-		{
-			this.dispose();
-		}
 		
 		if (e.getSource() instanceof FieldButton){
 			FieldButton button = (FieldButton) e.getSource();
@@ -386,6 +422,42 @@ public class ReversiGUI extends JFrame implements ActionListener {
 			}		
 			
 		}
+		
+		if(e.getSource() == this.menu)	//menu
+		{
+			if(!this.controlPanel.isShowing())
+			{
+				this.getContentPane().remove(this.boardPanel);
+				this.getContentPane().add(controlPanel, "cell 0 0,baseline");
+				this.controlPanel.setVisible(true);
+			}
+			else
+			{
+				this.getContentPane().remove(this.controlPanel);
+				this.getContentPane().add(boardPanel, "cell 0 0,baseline");
+				this.controlPanel.setVisible(false);
+			}
+			this.revalidate();
+			this.repaint();
+		}
+		if(e.getSource() == this.freezer)
+		{
+			if(freezer.isSelected())
+			{
+				this.spinB.setEnabled(true);
+				this.spinC.setEnabled(true);
+				this.spinI.setEnabled(true);
+				this.spinB.setValue(this.B);
+				this.spinC.setValue(this.C);
+				this.spinI.setValue(this.I);
+			}
+			else
+			{
+				this.spinB.setEnabled(false);
+				this.spinC.setEnabled(false);
+				this.spinI.setEnabled(false);
+			}
+		}
 	}
 
 	public int getBoardSize() {
@@ -394,9 +466,9 @@ public class ReversiGUI extends JFrame implements ActionListener {
 
 	private void setColor(int i, int j, Boolean white){
 		if (white){
-			btnFields[i-1][j-1].setBackground(Color.WHITE);
+			btnFields[i-1][j-1].setIcon(this.whiteStone);
 		} else {
-			btnFields[i-1][j-1].setBackground(Color.BLACK);
+			btnFields[i-1][j-1].setIcon(this.blackStone);
 		}
 	}
 	
@@ -437,12 +509,12 @@ public class ReversiGUI extends JFrame implements ActionListener {
 	protected void gameOver(){
 		playing = false;
 		info.setText("GAMEOVER");
+		JOptionPane.showMessageDialog(new JFrame(), "You won ?"); //TODO: prehral som 
 		for	(int i=0; i<boardSize; i++){
 			for	(int j=0; j<boardSize; j++){	
 				btnFields[i][j].setEnabled(false);
 			}
 		}
-		// TODO: vyskakovaci okno s vitezem
 		System.out.println("GAME OVER");
 	}
 	
@@ -457,8 +529,16 @@ public class ReversiGUI extends JFrame implements ActionListener {
 			boolean white = toTurn.getDisk().isWhite();
 			setColor(toTurn.getRow(), toTurn.getCol(), white);
 		}
-		// TODO: ZMENIT NA ODSTRANENI IKONKY!!!
-		btnFields[remove.getRow()-1][remove.getCol()-1].setBackground(new Color(0,102,153));
+		btnFields[remove.getRow()-1][remove.getCol()-1].setIcon(null);;
 		
+	}
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		new ReversiGUI(8, 1, 0, 0, 0);
 	}
 }
